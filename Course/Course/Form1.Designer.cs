ff93863f31408a67935744900fd564da661bce83
Adel-94 <A.Ismayilova94@hotmail.com>
2017-06-10 01:22:25 +0400

    Add files via upload

---
+++ Course/Course/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Course
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtFull = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cboxGroupName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(12, 363);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(105, 363);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(349, -10);
+            this.dataGridView1.MaximumSize = new System.Drawing.Size(341, 264);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(341, 264);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(341, 264);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 48);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.updateToolStripMenuItem.Text = "Changed";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // txtFull
+            // 
+            this.txtFull.Location = new System.Drawing.Point(80, 34);
+            this.txtFull.Name = "txtFull";
+            this.txtFull.Size = new System.Drawing.Size(100, 20);
+            this.txtFull.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(80, 81);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 4;
+            // 
+            // cboxGroupName
+            // 
+            this.cboxGroupName.FormattingEnabled = true;
+            this.cboxGroupName.Location = new System.Drawing.Point(80, 125);
+            this.cboxGroupName.Name = "cboxGroupName";
+            this.cboxGroupName.Size = new System.Drawing.Size(121, 21);
+            this.cboxGroupName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Fullname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "GroupName:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 422);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboxGroupName);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtFull);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtFull;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cboxGroupName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+    }
+}
+
---
+++ Course/Course/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course
+{
+    public partial class Form1 : Form
+    {  
+
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CourseDataContext db = new CourseDataContext();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowData();
+            Fillgroupname();
+        }
+
+        private void ShowData()
+        {
+            var myquery = from stu in db.Students join
+                          grp in db.Groups on
+                          stu.group_id equals grp.id
+                          select new
+                          {   
+                              stu.id,
+                              stu.fullname,
+                              stu.age,
+                              grp.name
+                          };
+            dataGridView1.DataSource = myquery;
+        }
+
+        void Fillgroupname()
+        {
+            var cmbquery = from grp in db.Groups
+                           select grp;
+            cboxGroupName.DataSource = cmbquery;
+            cboxGroupName.DisplayMember = "name";
+            cboxGroupName.ValueMember = "id";
+                           
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Student stu = new Student();
+            stu.fullname = txtFull.Text;
+            stu.age =Convert.ToInt32(txtAge.Text);
+            stu.group_id = (int)cboxGroupName.SelectedValue;
+            db.Students.InsertOnSubmit(stu);
+            db.SubmitChanges();
+            ShowData();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int id =int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            Student stu = db.Students.First(a => a.id == id);
+            db.Students.DeleteOnSubmit(stu);
+            db.SubmitChanges();
+            ShowData();
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            txtFull.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtAge.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            cboxGroupName.Text= dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            Student stu = db.Students.First(b => b.id == id);
+            stu.fullname = txtFull.Text;
+            stu.age = int.Parse(txtAge.Text);
+            stu.group_id = (int)cboxGroupName.SelectedValue;
+            db.SubmitChanges();
+            ShowData();
+
+        }
+    }
+}
